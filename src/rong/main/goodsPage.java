ddf293506f709757819bdd84fc2924ea9fdeb434
ronghui <381531043@qq.com>
2018-12-05 18:02:22 +0800

    商品维护

---
+++ src/rong/main/goodsPage.java
@@ -0,0 +1,12 @@
+package rong.main;
+
+import rong.tools.inputContent;
+import rong.main.mainPage;
+
+public final class goodsPage {
+	// …Ã∆∑Œ¨ª§
+	public static void addGoodsPage() {
+		
+		
+	}
+}
--- src/rong/main/mainPage.java
+++ src/rong/main/mainPage.java
@@ -1,11 +1,17 @@
 package rong.main;
 
 import rong.tools.inputContent;
+import rong.main.goodsPage;;
 
 public final class mainPage {
 
 	public static void main(String[] args) {
 
+		mainFun();
+
+	}
+
+	public static void mainFun() {
 		System.out.println("***************************");
 		System.out.println("***************************\n");
 		System.out.println("\t 1.商品维护\n");
@@ -28,9 +34,40 @@ public final class mainPage {
 					System.out.println("已经退出系统！！");
 					System.exit(0);
 					break;
+				case 1:
+					maintentenancePage();
+					break;
+				case 2:
+					break;
 				}
 			}
 		}
 	}
 
+	public static void maintentenancePage() {
+		System.out.println("***************************\n");
+		System.out.println("\t 1.添加商品\n");
+		System.out.println("\t 2.更改商品\n");
+		System.out.println("\t 3.删除商品\n");
+		System.out.println("\t 4.查询商品\n");
+		System.out.println("\t 5.显示所有商品\n");
+		System.out.println("***************************");
+		System.out.println("\n请输入选项,或者按 0 返回上一级菜单.");
+
+		while (true) {
+			String choice = inputContent.ScannerInfoString();
+			String regex = "[0-5]";
+			if (choice.matches(regex)) {
+				int info = Integer.parseInt(choice);
+				switch (info) {
+				case 0:
+					mainFun();
+					break;
+				case 1:
+					goodsPage.addGoodsPage();
+					break;
+				}
+			}
+		}
+	}
 }
